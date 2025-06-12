003a3b81d67350d0972ffe58210784170d38a2ae
Yoan <yoan.ivanov0007@gmail.com>
2025-06-12 20:10:52 +0300

    ChatContainer.jsx, added useChatStore and other files

---
+++ frontend/chatapp/src/components/MessageInput.jsx
@@ -0,0 +1,9 @@
+const MessageInput = () => {
+    const [text,setText] = useState("");
+    const [imagePreview, setImagePreview] = useState(null)
+    const fileInputRef = useRef(null);
+
+    return <div>MessageInput</div>
+
+};
+    export default MessageInput;
